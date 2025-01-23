c313501035f3bbafb88fb89246078438a57ad040
Rodolfo Alves <rodolfo.alves@impactzero.pt>
2025-01-23 09:29:39 +0000

    Initializatoin

---
+++ movies_soap/src/main/java/com/movies/movies_soap/MoviesSoapApplication.java
@@ -0,0 +1,13 @@
+package com.movies.movies_soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesSoapApplication.class, args);
+	}
+
+}
---
+++ movies_soap/src/test/java/com/movies/movies_soap/MoviesSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.movies_soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
